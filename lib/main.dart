1d64cf794beced1ec47e1f0cb9f353eb7966a1d2
EthanBagunas <epbagunas@gmail.com>
2023-11-24 17:37:55 +0800

    cans end to db

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 //import 'package:firebase_database/firebase_database.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:myapp/firebase_options.dart';
 
 void main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final contactController =TextEditingController();
+  final literController =TextEditingController();
+  
 
   @override
   Widget build(BuildContext context) {
@@ -26,33 +31,27 @@ class MyApp extends StatelessWidget {
               child: Column( // Wrap the existing container and the new button in a Column
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    margin: const EdgeInsets.all(100),
-                    padding: const EdgeInsets.all(10),
-                    color: const Color.fromARGB(255, 209, 205, 255),
-                    height: 100,
-                    width: 500,
-                    child: const Column(
-                        children: [
-                            TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
+                            TextFormField(
+                              controller: contactController,
+                              decoration: const InputDecoration(
                                 hintText: 'Enter your contact number',
                               ),
                             ),
-                            /*TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(),
+                            TextFormField(
+                              controller: literController ,
+                              decoration: const InputDecoration(
                                 hintText: 'Enter your number of liters',
                               ),
-                            ),*/
-                        ],
-                      ),
-                  ),
+                            ),
                   const SizedBox(height: 20), // Add some space between the TextField and the Button
                   ElevatedButton(
                     onPressed: () {
                       // Add the desired functionality when the button is pressed
+                      CollectionReference collRef= FirebaseFirestore.instance.collection('info');
+                      collRef.add({
+                        'contact': contactController.text,
+                        'liter': literController.text
+                      });
                     },
                     child: const Text('Submit'),
                   ),
